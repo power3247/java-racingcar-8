dea79bc9fdc5a2cd67b5ee989b233fbfa488ac6e
power <posmane5@gmail.com>
2025-10-27 23:18:42 +0900

    feat : Application 클래스 구현완료.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.controller.Game;
+import racingcar.view.Input;
+import racingcar.view.InputInterface;
+import racingcar.view.Output;
+import racingcar.view.OutputInterface;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputInterface input = new Input();
+        OutputInterface output = new Output();
+
+        Game game = new Game(input,output);
+        game.run();
     }
 }
